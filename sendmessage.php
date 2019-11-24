0a554b55b940f6ccb1925339b3d65339486a4564
T0oki <gdefour2002@gmail.com>
2019-11-25 00:05:44 +0100

    Delete sendmessage.php

--- sendmessage.php
+++
@@ -1,4 +0,0 @@
-<?php
-include(__DIR__.DIRECTORY_SEPARATOR.'php'.DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR.'Message.php');
-Message::sendMessage((string)htmlspecialchars($_POST['author']), (string) htmlspecialchars($_POST['message']));
-
